55818f87c96bd5a36eac99fe664d91047c8d422d
Marek Szczegodziński <marek.szczegodzinski@lppsa.com>
2023-03-07 22:35:01 +0100

    feat: add url cloud function and hello_function script
    issue: #task_3, #task_3_optional

---
+++ task_3_optional_function/hello_function.py
@@ -0,0 +1,15 @@
+def hello_function():
+  print("Hello from the other siiiiiiiiiide!")
+
+def my_function_with_args(name, surname):
+  print("Hello, it's me, %s %s"%(name, surname))
+
+def sum_two_numbers(a, b):
+  return a + b
+
+hello_function()
+
+my_function_with_args("Marek", "Szczegodziński")
+
+x = sum_two_numbers(1,2)
+print(x)
